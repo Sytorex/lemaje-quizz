b1b9d256fd6067066ab5259b07048a13a52623d3
Sytorex <68004414+Sytorex@users.noreply.github.com>
2024-10-23 18:57:29 +0200

    V1 init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import '../main.scss';
+
+    const IMAGE_URL_PNG = "https://cdn.discordapp.com/avatars/240125087649497089/a423484e618769bbedf416d06ffb0d7d?size=1024";
+    const TITLE = "Quizz";
+</script>
+
+<svelte:head>
+    <link rel="icon" href={IMAGE_URL_PNG} type="image/png" />
+	<title>{TITLE}</title>
+</svelte:head>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,56 @@
+<div id="home">
+    <div>
+        <h1>Bienvenue sur le site de Quizz</h1>
+        
+        <div class="container-round">
+            <div class="round">
+                <h2>Les 90 secondes</h2>
+                <a href="/90-secondes/user" target="_blank">Ouvrir la page user</a>
+                <a href="/90-secondes/admin" target="_blank">Ouvrir la page admin</a>
+            </div>
+    
+            <div class="round">
+                <h2>La finale</h2>
+                <a href="/finale/user" target="_blank">Ouvrir la page user</a>
+                <a href="/finale/admin" target="_blank">Ouvrir la page admin</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style lang="scss">
+    #home {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+
+        > div {
+            display: flex;
+            flex-direction: column;
+            gap: 20px;
+            width: 80%;
+            max-width: 800px;
+        }
+    }
+
+    h1 {
+        color: red;
+        text-align: center;
+    }
+
+    .container-round {
+        display: flex;
+        justify-content: space-around;
+
+        .round {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+            a {
+                margin: 10px;
+            }
+        }
+    }
+</style>
