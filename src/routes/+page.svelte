8bcbcaa03640faaf258e059ffff766fc4709e38f
Sytorex <hydro.tex27@gmail.com>
2024-10-31 13:16:47 +0100

    Changement d'extension de fichier .html

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,25 @@
+<script lang="ts">
+    import { dev } from '$app/environment';
+
+    function getHref(path: string) {
+        return dev ? path : `${path}.html`;
+    }
+</script>
+
 <section id="home">
     <h1>Bienvenue sur le site de Quizz</h1>
     
     <div class="container-round">
         <div class="round">
             <h2>Les 90 secondes</h2>
-            <a href="90s/user" target="_blank">Ouvrir la page user</a>
-            <a href="90s/admin" target="_blank">Ouvrir la page admin</a>
+            <a href={getHref('90s/user')} target="_blank">Ouvrir la page user</a>
+            <a href={getHref('90s/admin')} target="_blank">Ouvrir la page admin</a>
         </div>
 
         <div class="round">
             <h2>La finale</h2>
-            <a href="finale/user" target="_blank">Ouvrir la page user</a>
-            <a href="finale/admin" target="_blank">Ouvrir la page admin</a>
+            <a href={getHref('finale/user')} target="_blank">Ouvrir la page user</a>
+            <a href={getHref('finale/admin')} target="_blank">Ouvrir la page admin</a>
         </div>
     </div>
 
